a3acc878067e18fa8cb4803284512baa88e87d8b
Robert Vogel <1201528+osnard@users.noreply.github.com>
2022-01-14 15:52:19 +0100

    Add required `$wgAdditionalGroups`

--- bootstrap.php
+++ bootstrap.php
@@ -10,4 +10,5 @@ if ( defined( 'MWSTAKE_MEDIAWIKI_COMPONENT_UTILS_VERSION' ) ) {
 
 define( 'MWSTAKE_MEDIAWIKI_COMPONENT_UTILS_VERSION', '1.0.0' );
 
+$GLOBALS['wgAdditionalGroups'] = [];
 $GLOBALS['wgServiceWiringFiles'][] = __DIR__ . '/includes/ServiceWiring.php';
