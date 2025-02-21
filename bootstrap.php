778138f836fae47f025b4d3f8ba6c0a1e833a7f3
Robert Vogel <vogel@hallowelt.biz>
2025-02-21 17:33:50 +0100

    Bump version

--- bootstrap.php
+++ bootstrap.php
@@ -4,7 +4,7 @@ if ( defined( 'MWSTAKE_MEDIAWIKI_COMPONENT_UTILS_VERSION' ) ) {
 	return;
 }
 
-define( 'MWSTAKE_MEDIAWIKI_COMPONENT_UTILS_VERSION', '2.0.6' );
+define( 'MWSTAKE_MEDIAWIKI_COMPONENT_UTILS_VERSION', '2.0.9' );
 
 MWStake\MediaWiki\ComponentLoader\Bootstrapper::getInstance()
 ->register( 'utils', static function () {
